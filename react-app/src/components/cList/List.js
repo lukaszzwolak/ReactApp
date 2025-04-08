8a21ee5ba1b9f94f8ccbd1dcb774e4baab931ef5
lukaszzwolak <lukasz.zwolak1607@gmail.com>
2025-04-08 13:35:10 +0200

    Add dynamic column rendering using .map and explain importance of React keys

--- react-app/src/components/cList/List.js
+++ react-app/src/components/cList/List.js
@@ -2,6 +2,17 @@ import styles from './List.module.scss';
 import Column from '../fColumn/Column.js';
 
 const List = () => {
+    const columns = [
+        { id: 1, title: 'Books', icon: 'book' },
+        { id: 2, title: 'Movies', icon: 'film' },
+        { id: 3, title: 'Games', icon: 'gamepad' }
+    ];
+
+    setTimeout(() => {
+        columns.push({ id: 4, title: 'Test column' });
+        console.log(columns)
+    }, 2000);
+
     return (
         <div className={styles.list}>
             <header className={styles.header}>
@@ -13,12 +24,14 @@ const List = () => {
                 Interesting things I want to check out
             </p>
             <section className={styles.columns}>
-                <Column title="Books" icon="book" />
-                <Column title="Movies" icon="film" />
-                <Column title="Games" icon="gamepad" />
+                {columns.map(column => (
+                    <Column key={column.id} title={column.title} icon={column.icon} />
+                ))}
             </section>
         </div>
     );
+
+
 };
 
 export default List;
