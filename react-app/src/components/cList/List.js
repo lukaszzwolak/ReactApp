fd49deee1a8faa27ec8bd37e59c46a8675730419
lukaszzwolak <lukasz.zwolak1607@gmail.com>
2025-04-08 14:08:20 +0200

    Fix setTimeout loop with useEffect to trigger only on initial render

--- react-app/src/components/cList/List.js
+++ react-app/src/components/cList/List.js
@@ -1,6 +1,7 @@
 import styles from './List.module.scss';
 import Column from '../fColumn/Column.js';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 const List = () => {
     const [columns, setColumns] = useState([
@@ -9,10 +10,13 @@ const List = () => {
         { id: 3, title: 'Games', icon: 'gamepad' }
     ]);
 
-    setTimeout(() => {
-        columns.push({ id: 4, title: 'Test column' });
-        //console.log(columns)
-    }, 2000);
+    useEffect(() => {
+
+        setTimeout(() => {
+            setColumns([...columns, { id: 4, title: 'Test column' }]);
+        }, 2000);
+
+    }, []);
 
     return (
         <div className={styles.list}>
