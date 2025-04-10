bc80553077984b8874eafc9cc2024188b418bb7d
lukaszzwolak <lukasz.zwolak1607@gmail.com>
2025-04-10 13:00:32 +0200

    feat: implement card form and refactor columns, cleanup broken deps

--- react-app/src/components/fColumn/Column.js
+++ react-app/src/components/fColumn/Column.js
@@ -1,12 +1,21 @@
 import styles from './Column.module.scss'
+import Card from '../iCard/Card';
+import CardForm from '../jCardForm/CardForm';
 
 const Column = props => {
     return (
         <article className={styles.column}>
             <h2 className={styles.title}>
-                <span className={styles.icon + ' fa fa-' + props.icon}></span>
+                <span
+                    className={styles.icon + ' fa fa-' + props.icon}></span>
                 {props.title}
             </h2>
+
+
+            <ul className={styles.cards}>
+                {props.cards.map(card => <Card key={card.id} title={card.title} />)}
+            </ul>
+            <CardForm action={card => props.action(card, props.id)} />
         </article>
     );
 };
