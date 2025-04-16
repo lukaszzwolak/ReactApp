2c9e911e9b41b93b9ed6c6a2fa759db0324f20f0
lukaszzwolak <lukasz.zwolak1607@gmail.com>
2025-04-16 20:58:16 +0200

    refactor: use useSelector in Column to retrieve and filter cards from Redux

--- react-app/src/components/fColumn/Column.js
+++ react-app/src/components/fColumn/Column.js
@@ -1,19 +1,22 @@
-import styles from './Column.module.scss'
+import styles from './Column.module.scss';
 import Card from '../iCard/Card';
 import CardForm from '../jCardForm/CardForm';
+import { useSelector } from 'react-redux';
 
 const Column = props => {
+    const cards = useSelector(state =>
+        state.cards.filter(card =>
+            card.columnId === props.id));
+
     return (
         <article className={styles.column}>
             <h2 className={styles.title}>
-                <span
-                    className={styles.icon + ' fa fa-' + props.icon}></span>
+                <span className={styles.icon + ' fa fa-' + props.icon}></span>
                 {props.title}
             </h2>
 
-
             <ul className={styles.cards}>
-                {props.cards.map(card => (
+                {cards.map(card => (
                     <Card
                         key={card.id}
                         id={card.id}
@@ -22,13 +25,13 @@ const Column = props => {
                     />
                 ))}
             </ul>
+
             <button
                 className={styles.removeButton}
                 onClick={() => props.removeColumn(props.id)}
             >
                 ×
             </button>
-
 
             <CardForm columnId={props.id} action={props.addCard} />
         </article>
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -14,4 +14,4 @@ root.render(
       <App />
     </Provider>
   </React.StrictMode>,
-);
+); 
