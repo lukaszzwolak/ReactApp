35928d92bccff6d48bc21de4d4a870b579f5602e
lukaszzwolak <lukasz.zwolak1607@gmail.com>
2025-05-15 12:50:09 +0200

    structural changes in Column.js

--- src/components/fColumn/Column.js
+++ src/components/fColumn/Column.js
@@ -5,8 +5,8 @@ import { useSelector } from 'react-redux';
 
 const Column = props => {
     const cards = useSelector(state =>
-        state.cards.filter(card =>
-            card.columnId === props.id));
+        state.cards.filter(card => card.columnId === props.id)
+    );
 
     return (
         <article className={styles.column}>
