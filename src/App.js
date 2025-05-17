ba035c1eccf075b72979e0ce0b92d327dc22c0be
lukaszzwolak <lukasz.zwolak1607@gmail.com>
2025-05-17 19:18:20 +0200

    Render all views (Home, Favorite, About) on a single page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import Container from './components/gContainer/Container';
 import NavBar from './components/kNavBar/NavBar';
-import { Routes, Route } from 'react-router-dom';
 import Favorite from './components/lpages/Favorite';
 import About from './components/lpages/About';
 import Home from './components/lpages/Home';
-
 
 const App = () => {
   return (
     <main>
       <NavBar />
       <Container>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/favorite" element={<Favorite />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
+        <Home />
+        <Favorite />
+        <About />
       </Container>
     </main>
   );
